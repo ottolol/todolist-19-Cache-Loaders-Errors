38b52fb82447570393527bede335d99bce8a101f
ottolol <alexsveshnik@gmail.com>
2025-06-13 12:14:37 +0300

    4. Для отлавливания и положительного, и отрицательного результатов сразу после мутации, используется unwrap.
    Результат: при удалении тудулиста становятся недоступными действия с тудулистом

--- src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
@@ -1,9 +1,15 @@
 import { EditableSpan } from "@/common/components"
-import { useRemoveTodolistMutation, useUpdateTodolistTitleMutation } from "@/features/todolists/api/todolistsApi"
+import {
+  todolistsApi,
+  useRemoveTodolistMutation,
+  useUpdateTodolistTitleMutation,
+} from "@/features/todolists/api/todolistsApi"
 import { type DomainTodolist } from "@/features/todolists/model/todolists-slice"
 import DeleteIcon from "@mui/icons-material/Delete"
 import IconButton from "@mui/material/IconButton"
 import styles from "./TodolistTitle.module.css"
+import { useAppDispatch } from "@/common/hooks"
+import { RequestStatus } from "@/common/types"
 
 type Props = {
   todolist: DomainTodolist
@@ -15,8 +21,29 @@ export const TodolistTitle = ({ todolist }: Props) => {
   const [removeTodolist] = useRemoveTodolistMutation()
   const [updateTodolistTitle] = useUpdateTodolistTitleMutation()
 
+  // const deleteTodolist = () => {
+  //   removeTodolist(id)
+  // }
+  const dispatch = useAppDispatch()
+
+  const changeTodolistStatus = (entityStatus: RequestStatus) => {
+    dispatch(
+      todolistsApi.util.updateQueryData("getTodolists", undefined, (state) => {
+        const todolist = state.find((todolist) => todolist.id === id)
+        if (todolist) {
+          todolist.entityStatus = entityStatus
+        }
+      }),
+    )
+  }
+
   const deleteTodolist = () => {
+    changeTodolistStatus("loading")
     removeTodolist(id)
+      .unwrap()
+      .catch(() => {
+        changeTodolistStatus("idle")
+      })
   }
 
   const changeTodolistTitle = (title: string) => {
