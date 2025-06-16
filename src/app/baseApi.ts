9b73ce51761f5b40808caad8f448ed522862af27
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-06-16 08:11:39 +0000

    7. Глобальная обработка ошибок с помощью baseQuery - обработали fetchError (если не будет сети, то ошибка будет обработана).

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,15 +1,26 @@
 import { AUTH_TOKEN } from "@/common/constants"
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { selectAppError, setAppErrorAC } from "@/app/app-slice"
 
 export const baseApi = createApi({
   reducerPath: "todolistsApi",
   tagTypes: ["Todolist", "Task"],
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_URL,
-    prepareHeaders: (headers) => {
-      headers.set("API-KEY", import.meta.env.VITE_API_KEY)
-      headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
-    },
-  }),
+  baseQuery: async (args, api, extraOptions) => {
+    const result = await fetchBaseQuery({
+      baseUrl: import.meta.env.VITE_BASE_URL,
+      prepareHeaders: headers => {
+        headers.set('API-KEY', import.meta.env.VITE_API_KEY)
+        headers.set('Authorization', `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+      },
+    })(args, api, extraOptions)
+    
+    if (result.error) {
+      if (result.error.status === 'FETCH_ERROR') {
+        api.dispatch(setAppErrorAC({ error: result.error.error }))
+      }
+    }
+    
+    return result
+  },
   endpoints: () => ({}),
 })
